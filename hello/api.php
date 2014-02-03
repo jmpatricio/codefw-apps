84280491c576c1c7b9c78d472e9242724e6a388a
João Patrício <jmpatricio@gmail.com>
2014-02-03 22:02:55 +0000

    Working with a  simple api

--- hello/api.php
+++ hello/api.php
@@ -1,1 +1,30 @@
-<?php echo "CodeFW-API"; ?>
+<?php
+
+class hello_api{
+    function barber($params)
+    {
+        $params = json_decode($params);
+        $t1['text'] = "Item 1";
+        $t1['done'] = "false";
+        
+        $t2['text'] = "Item 2";
+        $t2['done'] = "true";
+        
+        $t3['text'] = "Item 3";
+        $t3['done'] = "false";
+        
+        $tasks[] = $t1;
+        $tasks[] = $t2;
+        $tasks[] = $t3;
+        
+        echo json_encode($tasks);
+    }
+}
+
+
+$obj = new hello_api();
+$method_name = $_GET['method'];
+$parameter = $_GET['params'];
+
+$obj->{$method_name}($parameter);
+?>

615ec87ad39b25450d1046974ed0b98590ac7c4e
João Patrício <jmpatricio@gmail.com>
2014-02-04 23:16:58 +0000

    Get api url by javascript

--- hello/api.php
+++ hello/api.php
@@ -1,29 +1,32 @@
 <?php
 
+
 class hello_api{
     function barber($params)
     {
         $params = json_decode($params);
         $t1['text'] = "Item 1";
         $t1['done'] = false;
-        
+
         $t2['text'] = "Item 2";
         $t2['done'] = true;
-        
+
         $t3['text'] = "Item 3";
         $t3['done'] = false;
-        
+
         $tasks[] = $t1;
         $tasks[] = $t2;
         $tasks[] = $t3;
-        
+
         echo json_encode($tasks);
     }
 }
 
-$obj = new hello_api();
-$method_name = $_GET['method'];
-$parameter = $_GET['params'];
+if (!empty($_GET['method'])) {
+    $obj = new hello_api();
+    $method_name = $_GET['method'];
+    $parameter = (empty($_GET['params'])) ? $_GET['params'] : null;
+    $obj->{$method_name}($parameter);
+}
 
-$obj->{$method_name}($parameter);
 ?>

70c62962c9b091fbb2ad078e7c2e069b16203ec7
andrejcbittencourt <andreb2890@yahoo.com.br>
2014-02-08 23:13:15 +0000

    fix attempt : angularjs include

--- hello/js/js.js
+++ hello/js/js.js
@@ -1,3 +1,11 @@
-function JSController() {
+/* 
+ * Custom js
+ *
+ * @author : andré Bittencourt, <andreb2890@yahoo.com.br>
+ */
+
+var customJS = angular.module('customJS', []);
+
+customJS.controller('jsController', function($scope) {
 	
-}
+});

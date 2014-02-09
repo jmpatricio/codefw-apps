3611e75dbb5722efc84d7781258f491a1be43832
João Patrício <jmpatricio@gmail.com>
2014-02-09 17:54:20 +0000

    Singlepage application example

--- hello/js/js.js
+++ hello/js/js.js
@@ -3,13 +3,44 @@
  *
  * @author : João Patrício
  */
+	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	var appDir = codeFW_getAppBaseDir();
+	var viewsDir = '/' + appDir + 'views/';
+	
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
 
+			// route for the home page
+			.when('/', {
+				templateUrl : viewsDir+'parts/home.html',
+				controller  : 'mainController'
+			})
 
-	var HelloCtrl = function($scope){
-		$scope.name = 'Hello || :P';
+			// route for the about page
+			.when('/about', {
+				templateUrl : viewsDir+'parts/about.html',
+				controller  : 'aboutController'
+			})
 
-		$scope.getName = function(){
-			return $scope.name;
-		}
-	}
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : viewsDir+'parts/contacts.html',
+				controller  : 'contactController'
+			});
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	scotchApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
 

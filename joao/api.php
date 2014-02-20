ae47fcaca6749ee43d7bd3c15d28612a23ec29dc
João Patrício <jmpatricio@gmail.com>
2014-02-20 22:15:01 +0000

    Api for messages done - ver 1

--- joao/api.php
+++ joao/api.php
@@ -7,15 +7,78 @@ include '../../codefw/class.CodeFW_App_API.php';
 
 class hello_api extends CodeFW_App_API {
 
+    const dbHost = '185.28.20.12';
+    const dbUser = 'u765608950_msgus';
+    const dbPass = 'Zxd8h8dvXW';
+    const dbName = 'u765608950_msg';
+    
+    private $db;
+    
     function hello_api(){
         parent::CodeFW_App_API();
+        $connectionParams = array(
+            'dbname' => self::dbName,
+            'user' => self::dbUser,
+            'password' => self::dbPass,
+            'host' => self::dbHost,
+            'driver' => 'pdo_mysql',
+        );
+        $this->db = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, null);
 
     }
 
-    function hello_world($params){
-        $arr['test'] = 'OK';
-        return json_encode($arr);
-    } 
+    function list_users($params){
+        $sql = "SELECT * FROM user";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $users = $stmt->fetchAll();
+        return json_encode($users);
+    }
+    
+    function get_user($params){
+        
+        $id = (isset($params['id'])) ? $params['id'] : false;
+        if ($id){
+            $sql = 'select * from user where id=?';
+            $user = $this->db->fetchAssoc($sql, array($id), 0);
+            if (!empty($user)){
+                return json_encode($user);
+            } else {
+                return json_encode(array('error'=>'User not found'));
+            }
+        }
+        return json_encode(array('error'=>'User not found'));
+    }
+    
+    function get_message($params){
+        $id = (isset($params['id'])) ? $params['id'] : false;
+        if ($id){
+            $sql = 'select * from message where id=?';
+            $message = $this->db->fetchAssoc($sql, array($id), 0);
+            if (!empty($message)){
+                return json_encode($message);
+            } else {
+                return json_encode(array('error'=>'Message not found'));
+            }
+        }
+        return json_encode(array('error'=>'Message not found'));
+    }
+    
+    function add_message($params){
+        
+        $message['id_from'] = $params['id_from'];
+        $message['id_to'] = $params['id_to'];
+        $message['content'] = $params['content'];
+        $message['_sent'] = $params['_sent'];
+        $message['_read'] = $params['_read'];
+        $this->db->insert('message',$message);
+        return json_encode($this->db->insert('message',$message));
+    }
+    
+    function list_messages($params){
+        $messages = $this->db->fetchAll("select * from message");
+        return json_encode($messages);
+    }
 
 }
 
